2c8285c29dabbde025f77c61f13d17a5d01fc984
ldco2016 <renaissance.scholar2012@gmail.com>
2024-08-17 09:52:58 -0500

    first commit

--- server/index.js
+++ server/index.js
@@ -16,20 +16,25 @@ const pgClient = new Pool({
   host: keys.pgHost,
   database: keys.pgDatabase,
   password: keys.pgPassword,
-  port: keys.pgPort
+  port: keys.pgPort,
+  ssl:
+    process.env.NODE_ENV !== "production"
+      ? false
+      : { rejectUnauthorized: false },
 });
-pgClient.on("error", () => console.log("Lost PG connection"));
 
-pgClient
-  .query("CREATE TABLE IF NOT EXISTS values (number INT)")
-  .catch(err => console.log(err));
+pgClient.on("connect", (client) => {
+  client
+    .query("CREATE TABLE IF NOT EXISTS values (number INT)")
+    .catch((err) => console.error(err));
+});
 
 // Redis Client Setup
 const redis = require("redis");
 const redisClient = redis.createClient({
   host: keys.redisHost,
   port: keys.redisPort,
-  retry_strategy: () => 1000
+  retry_strategy: () => 1000,
 });
 const redisPublisher = redisClient.duplicate();
 
@@ -65,6 +70,6 @@ app.post("/values", async (req, res) => {
   res.send({ working: true });
 });
 
-app.listen(5000, err => {
+app.listen(5000, (err) => {
   console.log("Listening");
 });
